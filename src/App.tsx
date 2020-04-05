ea3678c50f0f112bd398824a5beaba8bb2ed8a50
cyberspacedk <frontend.dk@gmail.com>
2020-04-05 12:43:29 +0300

    typed form and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; 
 
 import NavBar from './components/NavBar';
 import ToDoForm from './components/ToDoForm';
@@ -6,8 +6,19 @@ import ToDoList from './components/ToDoList';
 
 import {TodoItem} from './interfaces'
 
+declare var confirm: (question:string)=> boolean
+
 const App: React.FunctionComponent = ()=>{ 
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [todos, setTodos] = useState<TodoItem[]>([]); 
+
+  useEffect(() => {
+    const saved = JSON.parse(localStorage.getItem('todos') || '[]') as TodoItem[];
+    setTodos(saved);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addHandler = (title:string)=>{
     const newTodo : TodoItem= {
@@ -15,15 +26,33 @@ const App: React.FunctionComponent = ()=>{
       id: Date.now(),
       completed: false
     }
-    setTodos([newTodo, ...todos]);
+    setTodos(prev=> [newTodo, ...prev]);
   };
 
+  const toggleHandler =(id:number)=>{ 
+    const updated = todos.map(i=> {  
+      if(i.id === id)  i.completed = !i.completed 
+      return i;
+    }); 
+    setTodos(updated) 
+  }; 
+
+  const removeHandler = (id:number)=> {
+    const shouldRemove = confirm('Are you sure that you wand to delete item ?');
+    if(shouldRemove){
+      setTodos(prev=> prev.filter(i=> i.id !== id))
+    }
+  }
   return (
   <>
     <NavBar />
     <div className="container">
        <ToDoForm onAdd={addHandler}/>
-       <ToDoList todos={todos}/>
+       <ToDoList 
+        todos={todos} 
+        onToggle={toggleHandler} 
+        onRemove={removeHandler}
+       />
     </div>
   </>
   )
@@ -34,6 +63,14 @@ export default App;
 // React.FC or React.FunctionComponent 
 /* 
 типизирует компонент (функциональный / классовый)
+
+типизация методов window, например confirm 
+
+1 способ вызвать явно как метод гл. объекта
+  window.confirm('Are you sure that you wand to delete item ?');
+
+2. использовать синтаксис TS и объявить переменную и описать ее 
+  declare var confirm: (question:string)=> boolean
 
 
 */
--- src/components/ToDoList/component.tsx
+++ src/components/ToDoList/component.tsx
@@ -1,24 +1,40 @@
 import React, {useState, useRef} from 'react'; 
+import { TodoItem } from '../../interfaces';
 
 type ToDoListProps = {
-  todos: any[]
+  todos: TodoItem[]
+  onToggle(id:number): void
+  onRemove: (id:number)=> void
 }
 
 
-const ToDoList : React.FC <ToDoListProps>= ({todos}) => { 
+const ToDoList : React.FC <ToDoListProps>= ({todos, onRemove, onToggle}) => { 
  
+  if(!todos.length){
+    return <p className="center">No items yet</p>
+  }
+
+  const removeHandler = (e: React.MouseEvent, id:number)=>{
+    e.preventDefault();
+    onRemove(id)
+  }
 
   return ( 
    <ul>
-     {todos.map(i=> (
-        <li className="todo">
+     {todos.map(i=> {
+       const classes = ['todo']; 
+       if(i.completed){
+         classes.push('completed')
+       }
+       return(
+        <li className={classes.join(' ')} key={i.id}>
           <label>
-            <input type="checkbox" name="" id=""/>
-            <span></span>
-            <i className="material-icons red-text">delete</i>
+            <input type="checkbox" checked={i.completed} onChange={()=>onToggle(i.id)}/>
+            <span>{i.title}</span>
+            <i className="material-icons red-text" onClick={(e)=>removeHandler(e, i.id)}>delete</i>
           </label>
         </li>
-     ))} 
+     )})} 
    </ul>
   )
 }
@@ -26,3 +42,16 @@ const ToDoList : React.FC <ToDoListProps>= ({todos}) => {
 export default ToDoList;
 
  
+/* 
+Хендлеры которые передаются в пропсы описываются несколькими способами
+
+  onRemove(id:number):void
+  
+  onRemove: (id:number)=> void
+
+если хендлеры необязательные то ставим ? 
+example:
+  onRemove?(id:number):void 
+  onRemove?: (id:number)=> void
+
+*/
